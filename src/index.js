5e00572c06bac38c53572ee4809181805b19918d
nbolognini <nbolognini@unimoron.edu.ar>
2024-02-07 13:38:18 -0300

    Probar Certificados BackEnd

--- src/index.js
+++ src/index.js
@@ -13,6 +13,24 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/', routes);
 
+
+
+
+// Agregado:
+const options = {
+    key: fs.readFileSync('etc/nginx/clave.privada.key'),
+    cert: fs.readFileSync('etc/nginx/clave.certificado.crt')
+};
+
+https.createServer(options, app).listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+// Hasta ACA
+
+
+
+
+
 app.listen(port, () => {    
     console.log(`Example app listening at http://localhost:${port}`);
 });
