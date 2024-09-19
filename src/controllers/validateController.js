e814aa450af1151871eb3900c11e07ff866ecf24
bolognini.ariel.universidad <bolognini.ariel.universidad@gmail.com>
2024-09-19 10:52:37 -0300

    Ok integracion con nueva base de datos, mens user inactivo

--- src/controllers/validateController.js
+++ src/controllers/validateController.js
@@ -3,7 +3,8 @@ const jwt = require('jsonwebtoken');
 
 module.exports.validate = (req, res) => {
     const { username, password, role } = req.body;
-    const consult = 'SELECT * FROM login WHERE username = ? AND password = ?';
+    // SI cambio paratros, o el nombre de la tabla, lo cambio en la variable consult
+    const consult = 'SELECT * FROM users WHERE user_name_id = ? AND user_password = ?';
 
     try {
         connection.query(consult, [username, password, role], (err, result) => {
@@ -12,13 +13,23 @@ module.exports.validate = (req, res) => {
             }
 
             if (result.length > 0) {
-                const token = jwt.sign({ username }, 'Stack', { expiresIn: '20m' });
-                //Acá generé la const role para agarre los datos traido de la base de datos
-                const role = result[0].role;
-                console.log(role);
-                //Si encuentro el user y el pass, devuelvo el token
-                //El user lo devuelve al front, pero no el role ¿?
-                return res.send({username, role, token });
+                //Si encuentro el user y el pass, verifico que user_status sea 1
+                if (result[0].user_status === 0) {
+                    console.log('Usuario inactivo');
+                    //Si retorna 0, devuelvo undefined al front para que no pueda loguearse
+                    return res.send(undefined);
+                }
+                else {
+                    console.log('Usuario activo');
+                    const token = jwt.sign({ username }, 'Stack', { expiresIn: '20m' });
+                    //Acá generé la const role para agarre los datos traido de la base de datos
+                    const role = result[0].user_role;
+                    console.log(role);
+                    //Si encuentro el user y el pass, devuelvo el token
+                    //Devuelvo el user, el role y el token
+                    return res.send({username, role, token });
+                }
+                
             } else {
                 console.log('Usuario o contraseña incorrectos');
                 //Si no encuentro el user y el pass, devuelvo undefined
